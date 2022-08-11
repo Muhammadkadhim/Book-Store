e512b59acbeccfaf635cd4069194d4d9dd8b3812
Muhammadkadhim <muhammad.kadhim.hassan@gmail.com>
2022-08-12 00:54:31 +0300

    completed: book details page

--- src/components/BookDetail.js
+++ src/components/BookDetail.js
@@ -1,33 +1,43 @@
 import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import axios from "axios";
+import { MdFavoriteBorder, MdOutlineShoppingCart } from "react-icons/md";
+import { Tooltip } from "flowbite-react";
 export default function BookDetail() {
     const { category, bookId } = useParams();
-    const [book, setBook] = useState({});
+    const [book, setBook] = useState();
 
     useEffect(() => {
         axios
-            .get(``)
+            .get(`https://openlibrary.org/works/${bookId}.json`)
             .then((data) => {
-                console.log(data);
-                // const books = data.data.works.map((work) => {
-                //     const { key, authors, cover_id, edition_count, title } =
-                //         work;
+                console.log(data.data);
+                let authors = [];
+                data.data.authors.forEach((author) => {
+                    axios
+                        .get(`https://openlibrary.org${author.author.key}.json`)
+                        .then((data) => {
+                            authors.push(data.data.name);
+                        })
+                        .catch((err) => console.log(err));
+                });
 
-                //     return {
-                //         id: key,
-                //         title: title,
-                //         authors: authors,
-                //         cover:
-                //             cover_id != null
-                //                 ? `https://covers.openlibrary.org/b/id/${cover_id}-L.jpg`
-                //                 : undefined,
-                //         edition_count: edition_count,
-                //         price: Math.floor(Math.random() * (15 - 5 + 1)) + 5,
-                //     };
-                // });
-
-                setBook(book);
+                // getting general data
+                setBook({
+                    id: data.data.key,
+                    title: data.data.title,
+                    authors: authors,
+                    cover: data.data.covers
+                        ? `https://covers.openlibrary.org/b/id/${data.data.covers[0]}-L.jpg`
+                        : undefined,
+                    description: data.data.description
+                        ? data.data.description.value
+                        : "No description found",
+                    subjects: data.data.subjects
+                        ? data.data.subjects.join(",")
+                        : "No subjects found",
+                    price: Math.floor(Math.random() * (15 - 5 + 1)) + 5,
+                });
             })
             .catch((err) => console.log(err));
     }, [bookId]);
@@ -42,196 +52,62 @@ export default function BookDetail() {
                     <li>
                         <Link to={`/category/${category}`}>{category}</Link>
                     </li>
-                    <li>{bookId}</li>
+                    <li>{book ? book.title : ""}</li>
                 </ul>
             </div>
-            {/* <div className=" flex flex-wrap items-center justify-center my-10">
-                {books ? (
-                    <BookList books={books} category={category} />
-                ) : (
-                    <Loading />
-                )}
-            </div> */}
+            {book ? (
+                <section className="body-font overflow-hidden bg-base-200 md:h-[600px] w-11/12 mx-auto my-5 rounded-lg flex items-center justify-center">
+                    <div className="w-9/12 px-5 py-5 mx-auto ">
+                        <div className="lg:w-4/5 mx-auto flex flex-wrap rounded-lg">
+                            <img
+                                alt={book.title}
+                                className="lg:w-1/2 w-full h-[400px] object-contain object-center rounded-lg"
+                                src={book.cover}
+                            />
+                            <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+                                <h2 className="text-sm title-font text-orange-400 tracking-widest">
+                                    {category}
+                                </h2>
+                                <h1 className="text-slate-100 text-2xl title-font font-medium mb-1">
+                                    {book.title}
+                                </h1>
+                                <hr className="my-5" />
+                                <h2 className="text-sm title-font text-orange-400 tracking-widest">
+                                    Description:
+                                </h2>
+                                <p className="leading-relaxed text-orange-200">
+                                    {book.description}
+                                </p>
+
+                                <div className="flex mt-5 justify-between items-center">
+                                    <span className="title-font font-medium text-2xl text-slate-400">
+                                        ${book.price}.00
+                                    </span>
+                                    <div className="flex gap-5 items-center">
+                                        <Tooltip content="Add to Cart">
+                                            <button className="text-orange-300">
+                                                <MdOutlineShoppingCart
+                                                    style={{ fontSize: "24px" }}
+                                                />
+                                            </button>
+                                        </Tooltip>
+
+                                        <Tooltip content="Add to Favorites">
+                                            <button className="text-orange-300">
+                                                <MdFavoriteBorder
+                                                    style={{ fontSize: "24px" }}
+                                                />
+                                            </button>
+                                        </Tooltip>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            ) : (
+                ""
+            )}
         </>
-        // <section class="text-gray-700 body-font overflow-hidden bg-white">
-        //     <div class="container px-5 py-24 mx-auto">
-        //         <div class="lg:w-4/5 mx-auto flex flex-wrap">
-        //             <img
-        //                 alt="ecommerce"
-        //                 class="lg:w-1/2 w-full object-cover object-center rounded border border-gray-200"
-        //                 src="https://www.whitmorerarebooks.com/pictures/medium/2465.jpg"
-        //             />
-        //             <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-        //                 <h2 class="text-sm title-font text-gray-500 tracking-widest">
-        //                     BRAND NAME
-        //                 </h2>
-        //                 <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">
-        //                     The Catcher in the Rye
-        //                 </h1>
-        //                 <div class="flex mb-4">
-        //                     <span class="flex items-center">
-        //                         <svg
-        //                             fill="currentColor"
-        //                             stroke="currentColor"
-        //                             stroke-linecap="round"
-        //                             stroke-linejoin="round"
-        //                             stroke-width="2"
-        //                             class="w-4 h-4 text-red-500"
-        //                             viewBox="0 0 24 24"
-        //                         >
-        //                             <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-        //                         </svg>
-        //                         <svg
-        //                             fill="currentColor"
-        //                             stroke="currentColor"
-        //                             stroke-linecap="round"
-        //                             stroke-linejoin="round"
-        //                             stroke-width="2"
-        //                             class="w-4 h-4 text-red-500"
-        //                             viewBox="0 0 24 24"
-        //                         >
-        //                             <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-        //                         </svg>
-        //                         <svg
-        //                             fill="currentColor"
-        //                             stroke="currentColor"
-        //                             stroke-linecap="round"
-        //                             stroke-linejoin="round"
-        //                             stroke-width="2"
-        //                             class="w-4 h-4 text-red-500"
-        //                             viewBox="0 0 24 24"
-        //                         >
-        //                             <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-        //                         </svg>
-        //                         <svg
-        //                             fill="currentColor"
-        //                             stroke="currentColor"
-        //                             stroke-linecap="round"
-        //                             stroke-linejoin="round"
-        //                             stroke-width="2"
-        //                             class="w-4 h-4 text-red-500"
-        //                             viewBox="0 0 24 24"
-        //                         >
-        //                             <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-        //                         </svg>
-        //                         <svg
-        //                             fill="none"
-        //                             stroke="currentColor"
-        //                             stroke-linecap="round"
-        //                             stroke-linejoin="round"
-        //                             stroke-width="2"
-        //                             class="w-4 h-4 text-red-500"
-        //                             viewBox="0 0 24 24"
-        //                         >
-        //                             <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-        //                         </svg>
-        //                         <span class="text-gray-600 ml-3">
-        //                             4 Reviews
-        //                         </span>
-        //                     </span>
-        //                     <span class="flex ml-3 pl-3 py-2 border-l-2 border-gray-200">
-        //                         <a class="text-gray-500">
-        //                             <svg
-        //                                 fill="currentColor"
-        //                                 stroke-linecap="round"
-        //                                 stroke-linejoin="round"
-        //                                 stroke-width="2"
-        //                                 class="w-5 h-5"
-        //                                 viewBox="0 0 24 24"
-        //                             >
-        //                                 <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-        //                             </svg>
-        //                         </a>
-        //                         <a class="ml-2 text-gray-500">
-        //                             <svg
-        //                                 fill="currentColor"
-        //                                 stroke-linecap="round"
-        //                                 stroke-linejoin="round"
-        //                                 stroke-width="2"
-        //                                 class="w-5 h-5"
-        //                                 viewBox="0 0 24 24"
-        //                             >
-        //                                 <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-        //                             </svg>
-        //                         </a>
-        //                         <a class="ml-2 text-gray-500">
-        //                             <svg
-        //                                 fill="currentColor"
-        //                                 stroke-linecap="round"
-        //                                 stroke-linejoin="round"
-        //                                 stroke-width="2"
-        //                                 class="w-5 h-5"
-        //                                 viewBox="0 0 24 24"
-        //                             >
-        //                                 <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-        //                             </svg>
-        //                         </a>
-        //                     </span>
-        //                 </div>
-        //                 <p class="leading-relaxed">
-        //                     Fam locavore kickstarter distillery. Mixtape
-        //                     chillwave tumeric sriracha taximy chia microdosing
-        //                     tilde DIY. XOXO fam indxgo juiceramps cornhole raw
-        //                     denim forage brooklyn. Everyday carry +1 seitan
-        //                     poutine tumeric. Gastropub blue bottle austin
-        //                     listicle pour-over, neutra jean shorts keytar banjo
-        //                     tattooed umami cardigan.
-        //                 </p>
-        //                 <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5">
-        //                     <div class="flex">
-        //                         <span class="mr-3">Color</span>
-        //                         <button class="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
-        //                         <button class="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
-        //                         <button class="border-2 border-gray-300 ml-1 bg-red-500 rounded-full w-6 h-6 focus:outline-none"></button>
-        //                     </div>
-        //                     <div class="flex ml-6 items-center">
-        //                         <span class="mr-3">Size</span>
-        //                         <div class="relative">
-        //                             <select class="rounded border appearance-none border-gray-400 py-2 focus:outline-none focus:border-red-500 text-base pl-3 pr-10">
-        //                                 <option>SM</option>
-        //                                 <option>M</option>
-        //                                 <option>L</option>
-        //                                 <option>XL</option>
-        //                             </select>
-        //                             <span class="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
-        //                                 <svg
-        //                                     fill="none"
-        //                                     stroke="currentColor"
-        //                                     stroke-linecap="round"
-        //                                     stroke-linejoin="round"
-        //                                     stroke-width="2"
-        //                                     class="w-4 h-4"
-        //                                     viewBox="0 0 24 24"
-        //                                 >
-        //                                     <path d="M6 9l6 6 6-6"></path>
-        //                                 </svg>
-        //                             </span>
-        //                         </div>
-        //                     </div>
-        //                 </div>
-        //                 <div class="flex">
-        //                     <span class="title-font font-medium text-2xl text-gray-900">
-        //                         $58.00
-        //                     </span>
-        //                     <button class="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">
-        //                         Button
-        //                     </button>
-        //                     <button class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-        //                         <svg
-        //                             fill="currentColor"
-        //                             stroke-linecap="round"
-        //                             stroke-linejoin="round"
-        //                             stroke-width="2"
-        //                             class="w-5 h-5"
-        //                             viewBox="0 0 24 24"
-        //                         >
-        //                             <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
-        //                         </svg>
-        //                     </button>
-        //                 </div>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </section>
     );
 }
