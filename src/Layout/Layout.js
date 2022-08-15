646ed112920909d3afca7e2f19e309e0bac6e29f
Muhammadkadhim <muhammad.kadhim.hassan@gmail.com>
2022-08-15 12:15:32 +0300

    temporary feature: screen size debugger

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,12 +1,13 @@
 import { Navbar, Footer } from "../components";
+import ScreenSize from "../components/Debug";
 
 export default function Layout({ children }) {
     return (
         <>
             <Navbar />
             <main className="my-24">{children}</main>
-
             <Footer />
+            <ScreenSize />
         </>
     );
 }
