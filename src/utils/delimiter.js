1aee815de43a606a4e546e0b061f22fe68ee9a37
Muhammad Kadhim <muhammad.kadhim.hassan@gmail.com>
2023-09-03 00:22:25 +0300

    Update delimiter.js

--- src/utils/delimiter.js
+++ src/utils/delimiter.js
@@ -1,5 +1,11 @@
 export const delimiter = (text, limit) => {
-    if (text.length <= limit) return text;
-
-    return `${text.substr(0, limit)}...`;
+    if (text && limit) {
+        if (text.length <= limit) {
+            return text;
+        } else {
+            return `${text.substr(0, limit)}...`;
+        }
+    } else {
+        return '';
+    }
 };
